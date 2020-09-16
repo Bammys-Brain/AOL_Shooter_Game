a3e0300c04cd8a7ff90c3c173c6752413186b93a
Bammys-Brain <70377429+Bammys-Brain@users.noreply.github.com>
2020-09-17 01:13:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -135,7 +135,7 @@ if (arrowGroup.isTouching(pinkB)) {
 function redBalloon() {
  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
   red.addImage(red_balloonImage);
-  red.velocityX = 3;
+  red.velocityX = 10;
   red.lifetime = width;
   red.scale = 0.2;
   redB.add(red);
@@ -146,7 +146,7 @@ function redBalloon() {
 function blueBalloon() {
   var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
   blue.addImage(blue_balloonImage);
-  blue.velocityX = 3;
+  blue.velocityX = 10;
   blue.lifetime = width;
   blue.scale = 0.2;
   blueB.add(blue);
@@ -157,7 +157,7 @@ function blueBalloon() {
 function greenBalloon() {
   var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
   green.addImage(green_balloonImage);
-  green.velocityX = 3;
+  green.velocityX = 10;
   green.lifetime = width;
   green.scale = 0.2;
   greenB.add(green);
@@ -168,7 +168,7 @@ function greenBalloon() {
 function pinkBalloon() {
   var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
   pink.addImage(pink_balloonImage);
-  pink.velocityX = 3;
+  pink.velocityX = 10;
   pink.lifetime = width;
   pink.scale = 0.2;
   pinkB.add(pink);
